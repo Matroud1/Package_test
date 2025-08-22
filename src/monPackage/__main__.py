ff0069f1f4293263a3c6b4479abbb9bbd3e72500
Matroud <madanyd58@gmail.com>
2025-08-22 06:08:56 +0200

    creation de nouveaux fichiers

---
+++ src/monPackage/__main__.py
@@ -0,0 +1,9 @@
+from monPackage import nettoyage, stats
+
+__main__="__main__"
+if __name__ == "__main__":
+    print("Nettoyage des données...")
+    nettoyage()
+    print("Calcul des statistiques...")
+    stats()
+    print("Opérations terminées.")
